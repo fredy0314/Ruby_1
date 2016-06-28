092fe26bd4a62ccf301b6937846eaf4edad18a7d
fredy0314 <fredyloaiza6@gmail.com>
2016-06-27 20:53:27 -0500

    inicio ruby

---
+++ operation_initial.rb
@@ -0,0 +1,19 @@
+class OperationInitial
+
+	def self.sum (num1,num2)
+		 num1 + num2
+	end
+
+
+	def self.res (num1,num2)
+		 num1 - num2
+	end
+
+
+	def self.mult (num1,num2)
+		 num1 * num2
+	end
+
+	
+
+ end
